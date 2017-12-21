33165c1c9332480823fdf5df0f1727d5e8d93b19
Mickael <brenoit.mickael@gmail.com>
2017-12-21 10:15:17 +0100

    Add a link XML to header

--- header.php
+++ header.php
@@ -45,6 +45,9 @@
                 <li class="nav-item">
                   <a class="nav-link" href="indexadmin.php">Manage Users</a>
                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">XML</a>
+                </li>
                 <?php } ?>
                 
             </ul>
